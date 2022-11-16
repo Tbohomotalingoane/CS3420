73dc825e334409ec199d7ebf28d26c1e10016ce1
Teboho <117016959+Tbohomotalingoane@users.noreply.github.com>
2022-11-16 16:12:58 +0200

    Update model.c

--- reversed prime squares/model.c
+++ reversed prime squares/model.c
@@ -1,21 +1,9 @@
 /*
-Author:			 Teboho Motalingoane
+Author:			Motalingoane Teboho
 Stundent ID:	202001038
-Email:			motalingoaneteboho21@gmail.com
-Contacts:		63464373/59802261
-Date:			30 October 2022
-Task:			Finding reversible prime squares
-*/
-
-#include <stdio.h>
-
-
-// The function reverses/*
-Author:			Teboho Motalingoane
-Stundent ID:	202001038
-Email:			motalingoaneteboho21@gmail.com
-Contacts:		63464373/59802261
-Date:			30 October 2022
+Email:			mmotalingoaneteboho21@gmail.com
+Contacts:		63464373/ 59802261
+Date:			28/10/ 2022
 Task:			Finding reversible prime squares
 */
 
@@ -23,78 +11,13 @@ Task:			Finding reversible prime squares
 
 
 // The function reverses the number
-int reverse(int number){
-	int  reverse = 0, remainder;					
-	while(num != 0)										
-	{
-		remainder = number % 10;
-		reverse = reverse * 10 + remainder;				
-		number = number / 10;								
-	}
- 	return reverse;
-}
-
-// The function determines prime numbers
-int is_prime(int num)
-{
-	// For every number less than 2 the function returns 0
-	if(number < 2)
-	{
-		return 0;
-	}
-	
-	for(int i = 2; i < num; i++)
-	{
-		if(number % i == 0)								// Checking whether num has factors 
-		{
-			return 0;									// If num has a factor the function returns 0
-		}
-	}
-	return 1;
-}
-
-// Finds and displays reversible prime squares
-void reversible_prime_sqrs()
-{
-	// Number and conut are initialised to zero
-	for(int num = 0, count = 0; count < 10; num++){
-
-		if(is_prime(num) == 1)						
-		{
-			int square = num*num;					
-			int reversed = reverse(square);				
-			if(square != reversed)						
-			{
-				for(int i = 0; i < reversed/2; i++)
-				{
-					if(i*i == reversed)					
-					{
-						if(is_prime(i) == 1)			
-						{
-							printf("\n%d",square);		
-							count ++;					
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
-int main()
-{
-    reversible_prime_sqrs();
-    
-    return 1;
-}
- the number
 int reverse(int num){
-	int  reverse = 0, remainder;						
-	while(num != 0)										
+	int  reverse = 0, remainder;						//Initalizing values
+	while(num != 0)										// Terminal condition
 	{
 		remainder = num % 10;
-		reverse = reverse * 10 + remainder;				
-		num = num / 10;									
+		reverse = reverse * 10 + remainder;				// reverse is stored in reverse variable
+		num = num / 10;									// Changing num by taking quotient of it divide by 10
 	}
  	return reverse;
 }
@@ -110,9 +33,9 @@ int is_prime(int num)
 	
 	for(int i = 2; i < num; i++)
 	{
-		if(num % i == 0)							
+		if(num % i == 0)								// Checking whether num has factors 
 		{
-			return 0;									
+			return 0;									// If num has a factor the function returns 0
 		}
 	}
 	return 1;
@@ -124,20 +47,20 @@ void reversible_prime_sqrs()
 	// Number and conut are initialised to zero
 	for(int number = 0, count = 0; count < 10; number++){
 
-		if(is_prime(number) == 1)						
+		if(is_prime(number) == 1)						// Determining whether the "number" is prime or not
 		{
-			int square = number*number;					
-			int reversed = reverse(square);				
-			if(square != reversed)				
+			int square = number*number;					// Squaring prime number
+			int reversed = reverse(square);				// Reversing the square of a prime
+			if(square != reversed)						// Checking for Palindrome
 			{
 				for(int i = 0; i < reversed/2; i++)
 				{
-					if(i*i == reversed)				
+					if(i*i == reversed)					// Determining a square
 					{
-						if(is_prime(i) == 1)		
+						if(is_prime(i) == 1)			// Checking whether i is prime or not 
 						{
-							printf("\n%d",square);		
-							count ++;					
+							printf("\n%d",square);		// Printing the reversible prime squares
+							count ++;					// Increamneting the counter
 						}
 					}
 				}
